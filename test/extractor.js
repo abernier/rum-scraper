16fefcd7c8703941388881cf9533b0d532dc3b92
Antoine BERNIER <antoine.bernier@gmail.com>
2013-06-16 16:59:26 +0200

    Welcome to Rumlola

--- test/extractor.js
+++ test/extractor.js
@@ -24,7 +24,7 @@ function scrap(url, username, password) {
 test("Seed from thumbnails", function (t) {
 	t.plan(2)
 
-	scrap('http://www.adopteunmec.com/', 'antoine.bernier@gmail.com', '292901').then(function (docs) {
+	scrap('http://www.adopteunmec.com/', 'antoine.bernier+rumlola@gmail.com', 'toto123').then(function (docs) {
 
 		// [{}, ...]
 		t.ok(_.isArray(docs) && docs.length > 0, "should be a not empty array");
@@ -51,18 +51,21 @@ test("Seed from thumbnails", function (t) {
 test("Consolidate from profile", function (t) {
 
 	//
-	// Male/Female
+	// Male/Female, test with Rumlola
 	//
 
 	t.test("male/female in common", function (t) {
 		t.plan(15); // in case scraper fails
 
-		scrap('http://www.adopteunmec.com/profile/17804907', 'antoine.bernier@gmail.com', '292901').then(function (docs) {
+		scrap('http://www.adopteunmec.com/profile/17824159', 'antoine.bernier+rumlola@gmail.com', 'toto123').then(function (docs) {
 			var doc = docs[0];
-			//console.log(docs);
+			console.log(doc);
 
 			// id
-			t.ok(''+doc.id === ''+17804907, "first doc should be 17804907");
+			t.ok(''+doc.id === ''+17824159, "first doc should be 17804907");
+
+			// pseudo
+			t.ok(_.isString(doc.pseudo) && doc.pseudo === "Rumlola", "pseudo is Rumlola")
 
 			// sex
 			t.ok(doc.sex.length, "sex should exist")
@@ -80,31 +83,38 @@ test("Consolidate from profile", function (t) {
 			t.ok(_.isNumber(doc.basket) && doc.basket >= 0, "basket should be a positive number")
 
 			// pics
-			t.ok(_.isArray(doc.pics) && doc.pics.length >= 1, "pics should be a not empty array")
+			t.ok(_.isArray(doc.pics) && doc.pics.length === 2, "Rumlola has 2 pics")
 
 			// big pics
 			t.ok(_.isArray(doc.bigPics) && doc.bigPics.length === doc.pics.length, "bigPics should be an array with same length than pics")
 
-			// pseudo
-			t.ok(_.isString(doc.pseudo) && doc.pseudo.length, "pseudo should be a not empty string")
-
 			// age
-			t.ok(_.isNumber(doc.age) && doc.age >= 0, "age should be a positive number")
+			function getAge(dateString) {
+		    var today = new Date();
+		    var birthDate = new Date(dateString);
+		    var age = today.getFullYear() - birthDate.getFullYear();
+		    var m = today.getMonth() - birthDate.getMonth();
+		    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+		      age--;
+		    }
+		    return age;
+			}
+			t.ok(doc.age === getAge("01/01/1981"), "Rumlola is born 01/01/1981")
 
 			// size
-			t.ok(_.isNumber(doc.size) && doc.size >= 0, "size should be a positive number")
+			t.ok(doc.size === 165, "Rumlola is 165cm")
 
 			// weight
-			t.ok(_.isNumber(doc.weight) && doc.weight >= 0, "weight should be a positive number")
+			t.ok(doc.weight === 65, "Rumlola is 65kg")
 
 			// geo
 			t.ok(_.isNumber(doc.geo.lat) && _.isNumber(doc.geo.lat), "geo should be {lat: <number>, lng: <number>}")
 
 			// description
-			t.ok(_.isString(doc.description) && doc.description.length, "description should be a not empty string")
+			t.ok(doc.description === "Petite nouvelle ici, apres tout, qui ne tente rien n'a rien : alors Messieurs, a vous de me découvrir ! ;)", "Rumlola description")
 
 			// shoppinglist
-			t.ok(_.isString(doc.shoppinglist) && doc.shoppinglist.length, "shoppinglist should be a not empty string")
+			t.ok(doc.shoppinglist === "Un physique avant tout, puis juste derriere une tres bonne situation sociale, enfin, peut-etre un peu de love :p", "Rumlola shoppinglist")
 
 			t.end()
 
@@ -115,18 +125,18 @@ test("Consolidate from profile", function (t) {
 	
 
 	//
-	// Female specific
+	// Female specific (Rumlola)
 	//
 
 	t.test("female specific", function (t) {
 		t.plan(2); // in case scraper fails
 
-		scrap('http://www.adopteunmec.com/profile/17804907', 'antoine.bernier@gmail.com', '292901').then(function (docs) {
+		scrap('http://www.adopteunmec.com/profile/17824159', 'antoine.bernier+rumlola@gmail.com', 'toto123').then(function (docs) {
 			var doc = docs[0];
-			// console.log(doc)
+			console.log(doc)
 
 			// sex
-			t.ok(doc.sex === 'female', "17804907 is a female")
+			t.ok(doc.sex === 'female', "Runlola is a female")
 
 			// charm
 			t.ok(_.isNumber(doc.charm) && doc.charm >= 0, "charm should be a positive number")
@@ -145,9 +155,9 @@ test("Consolidate from profile", function (t) {
 	t.test("male specific", function (t) {
 		t.plan(1); // in case scraper fails
 
-		scrap('http://www.adopteunmec.com/profile/26141696', 'antoine.bernier@gmail.com', '292901').then(function (docs) {
+		scrap('http://www.adopteunmec.com/profile/26141696', 'antoine.bernier+rumlola@gmail.com', 'toto123').then(function (docs) {
 			var doc = docs[0];
-			//console.log(doc)
+			console.log(doc)
 
 			// sex
 			t.ok(doc.sex === 'male', "26141696 is a male")
